3321444572eae874e138225cdc95e1322d4aafa6
coen-h <105825347+coen-h@users.noreply.github.com>
2025-04-04 17:19:15 +1300

    fixed

--- config/site.ts
+++ config/site.ts
@@ -13,17 +13,5 @@ export const siteConfig = {
       title: "Documents",
       href: "/documents",
     },
-    {
-      title: "Credentials",
-      href: "/credentials",
-    },
-    {
-      title: "Github",
-      href: "https://github.com/anis-marrouchi/chatpdf-gpt",
-    },
   ],
-  links: {
-    twitter: "https://twitter.com/anis_marrouchi",
-    github: "https://github.com/anis-marrouchi/chatpdf-gpt",
-  },
 }
